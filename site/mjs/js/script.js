85f665dbe7ff0ebebf4aa9ce8ae32be83c5e5014
novak.lukic <novak.lukic@alti.rs>
2016-07-27 09:44:04 +0200

    week 3 exercise

--- site/mjs/js/script.js
+++ site/mjs/js/script.js
@@ -1,4 +1,47 @@
-var menu = document.getElementsByTagName('h2');
-var meni=['Proja', 'Hleb'];
-var meni = meni.push('Sarma');
-console.log(meni[0]);
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+function loadFruits(){
+  document.getElementById("fruits").innerHTML = fruits;
+}
+
+function addFruit() {
+  var fruit = prompt("What is your favorite fruit?");
+  //fruits[fruits.length] = fruit;
+  fruits.push(fruit);
+  //document.getElementById("fruits").innerHTML = fruits;
+  loadFruits();
+}
+function removeFruit() {
+  fruits.pop();
+  loadFruits();
+}
+function removeFirstFruit() {
+  fruits.shift();
+  loadFruits();
+}
+
+var grades = [80, 87, ,'test' , , 98, 81, 81, 74, 91];
+var sum = 0;
+var count = 0;
+if(grades.length>0){
+  for(var i = 0; i < grades.length; i++) {
+    //if(grades[i] != undefined){
+    if(typeof grades[i] == "number"){
+     sum += grades[i];
+     count ++; 
+    }
+  }
+  //alert(sum/count);
+}
+else{
+  //alert("There is no numbers!");
+}
+
+var pass1= document.getElementById("pass1");
+var pass2= document.getElementById("pass2");
+function check(){
+  if (pass1.value != pass2.value){
+    alert("The password does not match");
+    return false;
+  }
+}
